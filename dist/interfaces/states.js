397931ea9a3ee852e9f4c1c0ac2ad09adef6b857
JFernando12 <fire.jf2012@gmail.com>
2023-07-07 10:29:27 -0600

    changes

--- dist/interfaces/states.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserState = void 0;
-var UserState;
-(function (UserState) {
-    UserState["new"] = "new";
-    UserState["chatgpt"] = "chatgpt";
-    UserState["agent"] = "agente";
-})(UserState = exports.UserState || (exports.UserState = {}));
--- dist/interfaces/templates.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Templates = void 0;
-var Templates;
-(function (Templates) {
-    Templates["welcomeMessage"] = "\u00A1Hola! Soy Lucy, el bot de The Dream Gift.\n\nTe ayudar\u00E9 a resolver todas tus preguntas, pero si deseas hablar con una persona, solo escribe \"agente\" y una persona de nuestro equipo se comunicar\u00E1 contigo.\n  \nElige una opci\u00F3n:\n1. Informaci\u00F3n de los productos.\n2. Consultar estatus de mi pedido.\n3. Hablar con un agente.\n  \n\u00A1Gracias por elegir The Dream Gift! \uD83D\uDE0A\uD83C\uDF81";
-    Templates["agentInit"] = "Ya le hemos hecho saber a un miembro del equipo que estas en espera, en cualquier momento estar\u00E1 contigo. Los horarios de atenci\u00F3n son de 10am a 7pm de Lunes a Viernes\n\nEn caso que quieras volver conmigo nuevamente, solo escribe \"Lucy\".";
-    Templates["chatGptInit"] = "Excelente eleccion, a partir de ahora puede preguntar cualquier cosa que desear saber acerca de nuestro productos";
-})(Templates = exports.Templates || (exports.Templates = {}));
